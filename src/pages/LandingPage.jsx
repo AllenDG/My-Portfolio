a45b9e63d316edf1cb5dd31df97c8e665fb46634
allenDG <allenwalterfiestada@gmail.com>
2024-11-02 12:43:09 +0800

    added new content

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,47 +1,9 @@
-import { Button } from "@/components/ui/button";
-import myImage from "@/assets/images/myImage.png";
+import ProfileInfo from "@/components/ProfileInfo";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col items-center justify-start p-4 min-h-screen">
-      {/* Profile Image and Info Container */}
-      <div className="flex items-start space-x-4 rounded-lg p-4 mt-10">
-        <div className="w-32 h-32 mb-4">
-          <img
-            src={myImage}
-            alt="Profile Image"
-            className="w-full h-full object-cover rounded-lg"
-          />
-        </div>
-
-        {/* Name and Location */}
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
-            Allen Walter De Guzman
-          </h1>
-          <div className="flex items-center text-gray-600 dark:text-gray-400 mt-1">
-            <p className="text-sm">Mangaldan, Pangasinan</p>
-          </div>
-          <p className="text-base text-gray-900 dark:text-gray-200 mt-1">
-            UI/UX Designer and Frontend Developer
-          </p>
-
-          {/* Buttons */}
-          <div className="flex mt-4 space-x-2">
-            <Button variant="" as="a" href="mailto:your-email@example.com">
-              Send Email
-            </Button>
-            <Button
-              variant="secondary"
-              as="a"
-              href="/path-to-your-cv.pdf"
-              download
-            >
-              Download CV
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <main className="w-full max-w-4xl mx-auto bg-slate-400">
+      <ProfileInfo />
+    </main>
   );
 }
