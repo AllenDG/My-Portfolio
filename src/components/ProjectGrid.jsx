1287668c40a98fa713f848ef438012824df0d9ae
allenDG <allenwalterfiestada@gmail.com>
2024-11-06 01:08:39 +0800

    added responsive

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -2,18 +2,16 @@ import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 export default function ProjectGrid({ projects }) {
   return (
-    <section className="border border-black-500 p-4 rounded-lg col-span-3 w-full">
-      <div className="flex justify-between items-center mb-4">
-        
-      </div>
-      <div className="grid grid-cols-2 gap-4">
+    <section className=" p-4 rounded-lg col-span-3 w-full">
+     
+      <div className="grid grid-cols-1  sm:grid-cols-2 gap-4">
         {projects.map((project) => (
           <div
             key={project.id}
-            className="border border-black-500 rounded-md p-4 transition-transform duration-200 hover:shadow-lg hover:-translate-y-1"
+            className="border border-black-500 rounded-md p-3 transition-transform duration-200 hover:shadow-lg hover:-translate-y-1 mb-4" // Add mb-4 for mobile margin
           >
             {/* Project Title */}
-            <h3 className="font-bold text-gray-800">{project.title}</h3>
+            <h3 className="font-bold">{project.title}</h3>
 
             {/* Project Description */}
             <p className="text-sm text-gray-500 mt-2">{project.description}</p>
