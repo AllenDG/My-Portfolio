5f84f5fd7308812068d806e6908305f88016083c
allenDG <allenwalterfiestada@gmail.com>
2024-11-23 00:11:36 +0800

    footer

---
+++ src/components/ui/footer/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className=" w-full max-w-3xl mx-auto py-8">
+      <div className="container mx-auto px-4">
+        {/* Divider */}
+        <div className="my-6 border-t border-gray-300"></div>
+
+        {/* Bottom Footer Content */}
+        <div className="text-left text-sm ">
+          <p>© 2024 AllenDG. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
